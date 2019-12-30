e51a483868131f91f5b66404ece7967525f6451d
Etienne Studer <etienne@studer.nu>
2019-12-30 19:51:08 +0100

    Prepare plugin for application on Project and Settings instances

--- src/main/java/nu/studer/gradle/buildscan/teamcity/TeamCityBuildScanPlugin.java
+++ src/main/java/nu/studer/gradle/buildscan/teamcity/TeamCityBuildScanPlugin.java
@@ -3,13 +3,14 @@ package nu.studer.gradle.buildscan.teamcity;
 import com.gradle.scan.plugin.BuildScanExtension;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.initialization.Settings;
 import org.gradle.util.GradleVersion;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.Method;
 
 @SuppressWarnings("unused")
-public class TeamCityBuildScanPlugin implements Plugin<Project> {
+public class TeamCityBuildScanPlugin implements Plugin<Object> {
 
     private static final String TEAMCITY_VERSION_ENV = "TEAMCITY_VERSION";
     private static final String GRADLE_BUILDSCAN_TEAMCITY_PLUGIN_ENV = "GRADLE_BUILDSCAN_TEAMCITY_PLUGIN";
@@ -19,7 +20,7 @@ public class TeamCityBuildScanPlugin implements Plugin<Project> {
     private static final String BUILD_SCAN_SERVICE_URL_MESSAGE_ARGUMENT_PREFIX = "BUILD_SCAN_URL:";
 
     @Override
-    public void apply(@Nonnull Project project) {
+    public void apply(@Nonnull Object object) {
         // abort if old Gradle version is not supported
         if (GradleVersion.current().getBaseVersion().compareTo(GradleVersion.version("5.0")) < 0) {
             throw new IllegalStateException("This version of the TeamCity build scan plugin is not compatible with Gradle < 5.0");
@@ -30,7 +31,16 @@ public class TeamCityBuildScanPlugin implements Plugin<Project> {
             return;
         }
 
-        init(project);
+        // handle plugin application to settings file and project file
+        if (object instanceof Settings) {
+            Settings settings = (Settings) object;
+            throw new IllegalStateException("The TeamCity build scan plugin can currently only be applied to Project instances");
+        } else if (object instanceof Project) {
+            Project project = (Project) object;
+            init(project);
+        } else {
+            throw new IllegalStateException("The TeamCity build scan plugin can only be applied to Settings and Project instances");
+        }
     }
 
     private void init(Project project) {
